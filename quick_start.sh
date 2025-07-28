280b9aef7cebe168a336fee013f03a50ff47414f
meixiaoran <mar12242001@gmail.com>
2025-07-28 14:52:04 +0800

    fix9

--- quick_start.sh
+++ quick_start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # 无限循环版本
-ckpt=100
-max_ckpt=900
+ckpt=200
+max_ckpt=1000
 
 while [ $ckpt -le $max_ckpt ]; do
     echo "[$(date '+%F %T')] 启动 python new_eval_pose.py ..."
-    python new_eval_pose.py --checkpoint /home/mar/ckpt/DIC_B/13.21.48_train_diffusion_transformer_hybrid_reach_target/checkpoints/${ckpt}.ckpt
+    python new_eval_pose.py --checkpoint /home/mar/ckpt/DiC/12.19.26_train_diffusion_transformer_hybrid_reach_target/checkpoints/${ckpt}.ckpt
     echo "[$(date '+%F %T')] train.py 已退出，等待 100 秒后重启 ..."
     sleep 15
     ((ckpt+=100))  # 增加100
