85e6bc8f20d42e2ddf734591835767c682c2cc9d
meixiaoran <mar12242001@gmail.com>
2025-08-03 14:55:40 +0800

    fix_bug

--- diffusion_policy/policy/diffusion_transformer_hybrid_image_policy.py
+++ diffusion_policy/policy/diffusion_transformer_hybrid_image_policy.py
@@ -104,7 +104,7 @@ class DiffusionTransformerHybridImagePolicy(BaseImagePolicy):
         #     obs_as_cond=obs_as_cond,
         #     n_cond_layers=n_cond_layers
         # )
-        model = DiC_S()
+        model = DiC_B()
         self.model = nn.ModuleDict({
             'obs_encoder': obs_encoder,
             'model': model
