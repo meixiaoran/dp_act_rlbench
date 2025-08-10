e24f41c705768fd48b1d066ab36a053655270b5d
meixiaoran <mar12242001@gmail.com>
2025-08-10 18:59:34 +0800

    aa

--- new_eval_pose.py
+++ new_eval_pose.py
@@ -370,8 +370,8 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description="Run diffusion policy with command-line arguments.")
-    parser.add_argument('--checkpoint', type=str, default='/home/mar/ckpt/open_box/dic_picture/checkpoints/800.ckpt')
-    parser.add_argument('--task_name', type=str, default='open_box')
+    parser.add_argument('--checkpoint', type=str, default='/home/mar/ckpt/take_umbrella/dic/checkpoints/300.ckpt')
+    parser.add_argument('--task_name', type=str, default='take_umbrella_out_of_umbrella_stand')
 
     # 也可以为其他参数添加默认值
     parser.add_argument('--ckpt_name0', type=str, default='latest.ckpt')
--- quick_start.sh
+++ quick_start.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # 无限循环版本，修改为每次增加50，遇到错误等待10分钟后重新执行
 
-ckpt=350
+ckpt=200
 max_ckpt=1000
 
 while [ $ckpt -le $max_ckpt ]; do
     echo "[$(date '+%F %T')] 启动 python new_eval_pose.py ..."
-    python new_eval_pose.py --checkpoint /home/mar/ckpt/DIC_S_25852/checkpoints/${ckpt}.ckpt
+    python new_eval_pose.py --checkpoint /home/mar/ckpt/take_umbrella/dic/checkpoints/${ckpt}.ckpt
     if [ $? -eq 0 ]; then
         echo "[$(date '+%F %T')] 训练完成，继续下一个ckpt."
-        ((ckpt+=50))  # 增加50
+        ((ckpt+=100))  # 增加50
         sleep 15
     else
         echo "[$(date '+%F %T')] 发生错误，等待10分钟后重试ckpt=${ckpt} ..."
