4b503473a0aa59fc8ca07287edf82a8e26de71ce
meixiaoran <mar12242001@gmail.com>
2025-08-12 18:29:06 +0800

    fudan1

--- new_eval_pose.py
+++ new_eval_pose.py
@@ -380,7 +380,7 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--no_onscreen_render', dest='onscreen_render', action='store_false')
     parser.set_defaults(onscreen_render=True)
-    parser.add_argument('--variation', type=int, default=1)
+    parser.add_argument('--variation', type=int, default=0)
     args = parser.parse_args()
 
     main(
